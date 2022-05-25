1401a57ee91a9be6e0ee88c4ee8440fd065e2386
Vaibhav-VTS <vaibhav.prasad@verifavia-shipping.com>
2022-05-25 18:44:36 +0530

    'portfolio'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react'
+import About from './components/About/About';
+import Homepage from './components/Homepage/Homepage'
+import Navbar from './components/Navbar/Navbar'
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
 
 function App() {
+
+  
+  
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#181a1c';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Navbar brand="Vaibhav" mode={mode} toggleMode={toggleMode} />
+        <Homepage homepageImage="../" mode={mode} />
+        <Portfolio mode={mode}/>
+        <About mode={mode}/>
+        <Contact mode={mode}/>
+    </>
+  )
 }
 
-export default App;
+export default App
